994ba8c1247b5ce58fe9ac5ad749089e7be4f5d6
Douae <douaeelyaaqoubi2@gmail.com>
2026-01-11 16:26:30 +0100

    new_project_modalJS

--- public/js/app.js
+++ public/js/app.js
@@ -1,70 +1,36 @@
 
-let user_email = document.getElementById("email");
-let user_password = document.getElementById("password");
-let signUpBtn = document.getElementById("signUpBtn");
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
 
 
-let emailMsg = document.createElement("p");
-emailMsg.textContent = "Email invalid";
-emailMsg.style.color = "red";
-emailMsg.style.fontSize = "14px";
-emailMsg.style.marginTop = "5px";
-emailMsg.style.display = "none";
-user_email.parentElement.parentElement.after(emailMsg);
-
-let passwordMsg = document.createElement("p");
-passwordMsg.textContent = "Password invalid";
-passwordMsg.style.color = "red";
-passwordMsg.style.fontSize = "14px";
-passwordMsg.style.marginTop = "5px";
-passwordMsg.style.display = "none";
-user_password.parentElement.parentElement.after(passwordMsg);
-
-function SING_UP() {
-    let emailValue = user_email.value.trim();
-    let passwordValue = user_password.value;
-
-    let emailValid = emailValue.includes("@") && emailValue.includes(".") && emailValue.length >= 10;
-    let passwordValid = passwordValue.length >= 7 && !passwordValue.includes(" ") && ["@", "#", "-", "+", "*", "/"].some(s => passwordValue.includes(s));
-
-    if (!emailValid) {
-        emailMsg.style.display = "block";
-    } else {
-        emailMsg.style.display = "none";
-    }
-
-    
-    if (!passwordValid) {
-        passwordMsg.style.display = "block";
-    } else {
-        passwordMsg.style.display = "none";
-    }
-
-    if (emailValid && passwordValid) {
-        window.location.href = "success.html";
-        return true;
-    }
-    return false;
-}
+const modal1 = document.getElementById('modal1');
+const modal2 = document.getElementById('modal2');
 
 
-let svg = document.getElementById("svg")
-
-svg.addEventListener("click", () => {
-    if (user_password.type == "password"){
-    user_password.type = "text"
-    }
-    else {
-        user_password.type = "password"
-    }
-})
+const closeBtns = document.querySelectorAll('.close');
 
 
-signUpBtn.addEventListener("click", (e) => {
-    e.preventDefault(); 
-    SING_UP();
+btn1.addEventListener('click', () => {
+    modal1.style.display = 'flex';
+    modal2.style.display = 'none';
 });
 
 
+btn2.addEventListener('click', () => {
+    modal2.style.display = 'flex';
+    modal1.style.display = 'none';
+});
 
 
+closeBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modal1.style.display = 'none';
+        modal2.style.display = 'none';
+    });
+});
+
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal1) modal1.style.display = 'none';
+    if (e.target === modal2) modal2.style.display = 'none';
+});
